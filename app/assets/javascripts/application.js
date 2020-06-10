b82571ef1ca87ecb0c163e2c118ee62b04c1a096
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-06-10 21:32:17 +0900

    [add]gem 'acts-as-taggable-on'

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
     moveSlides: 1,         // 移動するスライド数
     pause: 6000,           // 自動スライドの待ち時間
     maxSlides: 1,          // 一度に表示させる最大数
-    slideWidth: 1000,      // 各スライドの幅
+    slideWidth: 700,      // 各スライドの幅
 	randomStart: true,     // 最初に表示するスライドをランダムに設定
     autoHover: true,       // ホバー時に自動スライドを停止
     mode: 'fade',          
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
 	root "homes#home"
 	get "/about", to:"homes#about"
 	devise_for :users
+	resources :posts, only:[:new, :create, :index, :show]
 
 	devise_for :admins
 	namespace :admins do
 		root "homes#home"
+		resources :categories, only:[:index, :create, :destroy]
 	end
 
 end
