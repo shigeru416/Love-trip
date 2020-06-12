37dcd072b7955cc36788db0c2bb515e6b340344d
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-06-13 04:43:55 +0900

    [edit]comment_function

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,18 +1,17 @@
 class PostCommentsController < ApplicationController
 
 	def create
-		post = Post.find(params[:post_id])
-		comment = current_user.post_comments.new(post_comment_params)
-		comment.post_id = post.id
-		comment.save
-		redirect_back(fallback_location: root_path)
-	end
+		@post = Post.find(params[:post_id])
+	    @Post_new = Post.new
+	    @post_comment = @post.post_comments.new(post_comment_params)
+	    @post_comment.user_id = current_user.id
+	    @post_comment.save
+	    redirect_back(fallback_location: root_path)
+    end
 
 	def destroy
-		post = Post.find(params[:post_id])
-		comment = current_user.post_comments.find_by(post_id: post.id)
-		comment.post_id = post.id
-		comment.destroy
+		@post_comment = PostComment.find(params[:post_id])
+		@post_comment.destroy
 		redirect_back(fallback_location: root_path)
 	end
 
@@ -20,4 +19,5 @@ class PostCommentsController < ApplicationController
 	def post_comment_params
 		params.require(:post_comment).permit(:comment)
 	end
+
 end
