6d7367b907523f7e42be7a2368006bc37e2205a9
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-06-13 01:28:10 +0900

    [add]favorite_wannago_Asynchronous_communication

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,11 @@ class FavoritesController < ApplicationController
 		@post = Post.find(params[:post_id])
 		favorite = current_user.favorites.new(post_id: @post.id)
 		favorite.save
-		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
 		@post = Post.find(params[:post_id])
 		favorite = current_user.favorites.find_by(post_id: @post.id)
 		favorite.destroy
-    	redirect_back(fallback_location: root_path)
 	end
 end
--- app/controllers/wannago_lists_controller.rb
+++ app/controllers/wannago_lists_controller.rb
@@ -4,14 +4,12 @@ class WannagoListsController < ApplicationController
 		@post = Post.find(params[:post_id])
 		wannago_list = current_user.wannago_lists.new(post_id: @post.id)
 		wannago_list.save
-		redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
 		@post = Post.find(params[:post_id])
 		wannago_list = current_user.wannago_lists.find_by(post_id: @post.id)
 		wannago_list.destroy
-    	redirect_back(fallback_location: root_path)
 	end
 
 end
