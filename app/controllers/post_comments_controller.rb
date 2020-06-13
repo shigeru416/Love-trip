f433cc8f0a16fb5f9c6f4e63f1f7cf09c8c9a60d
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-06-13 14:07:19 +0900

    deploy setting

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -6,13 +6,11 @@ class PostCommentsController < ApplicationController
 	    @post_comment = @post.post_comments.new(post_comment_params)
 	    @post_comment.user_id = current_user.id
 	    @post_comment.save
-	    redirect_back(fallback_location: root_path)
     end
 
 	def destroy
 		@post_comment = PostComment.find(params[:post_id])
 		@post_comment.destroy
-		redirect_back(fallback_location: root_path)
 	end
 
 	private
